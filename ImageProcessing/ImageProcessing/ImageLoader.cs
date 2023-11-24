696e52f2bb11abafc696358b1dde75734d11f6d8
Еремин Илья Викторович <i.eremin@kontur>
2023-11-24 10:59:01 +0500

    Init

---
+++ ImageProcessing/ImageProcessing/ImageLoader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace ImageProcessing
+{
+	public static class ImageLoader
+	{
+
+		/// <summary>
+		/// Performs given action on extracted bitmap data. After action is performed, bitmap is unlocked in memory 
+		/// (so bitmap data should not be referenced outside of the action)
+		/// </summary>
+		/// <param name="bmp">The bitmap to process</param>
+		/// <param name="bitmapDataAction">Action to perform</param>
+		public static T WithBitmapData<T>(this Bitmap bmp, Func<BitmapData, T> bitmapDataAction)
+		{
+			var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+			var bitmapData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+			try
+			{
+				return bitmapDataAction(bitmapData);
+			}
+			finally
+			{
+				bmp.UnlockBits(bitmapData);
+			}
+		}
+
+		/// <summary>
+		/// Performs given action on extracted bitmap data. After action is performed, bitmap is unlocked in memory 
+		/// (so bitmap data should not be referenced outside of the action)
+		/// </summary>
+		/// <param name="bmp">The bitmap to process</param>
+		/// <param name="bitmapDataAction">Action to perform</param>
+		public static void WithBitmapData(this Bitmap bmp, Action<BitmapData> bitmapDataAction)
+		{
+			WithBitmapData(bmp, bitmapData =>
+			{
+				bitmapDataAction(bitmapData);
+				return 0;
+			});
+		}
+
+		/// <summary>
+		/// Set colors in bitmap
+		/// </summary>
+		/// <param name="bmp">Bitmap to set</param>
+		/// <param name="colors">Seted colors</param>
+		public static void SetColorValues(this Bitmap bmp, byte[] colors)
+		{
+			bmp.WithBitmapData(bmpData => Marshal.Copy(colors, 0, bmpData.Scan0, colors.Length));
+		}
+
+		/// <summary>
+		/// Get colors in bitmaps format
+		/// </summary>
+		/// <param name="bmpData">Bitmap Data</param>
+		/// <returns>Colors in bitmaps format</returns>
+		public static byte[] CopyRasterData(this BitmapData bmpData)
+		{
+			var size = bmpData.Stride * bmpData.Height;
+			var pointer = bmpData.Scan0;
+
+			var colorValues = RGBImage.AllocateImageData(bmpData.Stride, bmpData.Height);
+
+			Marshal.Copy(pointer, colorValues, 0, size);
+			return colorValues;
+		}
+	}
+}
---
+++ ImageProcessing/ImageProcessing/RGBImage.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ImageProcessing
+{
+	public class RGBImage
+	{
+		public int Width { get; }
+
+		public int Height { get; }
+
+		public byte[] ImageData { get; }
+
+		public int BytesPerLine { get; }
+
+		public const int BytesPerPixel = 3;
+
+		public RGBImage(int width, int height, int bytesPerLine, byte[] imageData)
+		{
+			Width = width;
+			Height = height;
+			BytesPerLine = bytesPerLine;
+			ImageData = imageData;
+		}
+
+		/// <summary>
+		/// Creates a new image object from file
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		public static RGBImage FromFile(string path)
+		{
+			using (var image = Image.FromFile(path))
+			{
+				using (var sourceBitmap = image as Bitmap ?? new Bitmap(image))
+				{
+					return FromBitmap(sourceBitmap);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Creates a new image object from the specified file
+		/// </summary>
+		/// <param name="data">Stream with an image content</param>
+		/// <returns></returns>
+		public static RGBImage FromStream(Stream data)
+		{
+			using (var image = Image.FromStream(data))
+			{
+				using (var sourceBitmap = image as Bitmap ?? new Bitmap(image))
+				{
+					return FromBitmap(sourceBitmap);
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// Creates a new image object from the given bitmap object
+		/// </summary>
+		/// <param name="bitmap"></param>
+		/// <returns></returns>
+		public static RGBImage FromBitmap(Bitmap bitmap)
+		{
+			return bitmap.WithBitmapData(GetBitmapDataAsIs);
+		}
+
+		/// <summary>
+		/// Stores this image to a file
+		/// </summary>
+		/// <param name="destPath">The path where the image has to be stored</param>
+		public void SaveToFile(string destPath)
+		{
+			using (var bitmap = ToBitmap())
+			{
+				bitmap.Save(destPath);
+			}
+		}
+
+		public static byte[] AllocateImageData(int stride, int height)
+		{
+			var dataSize = height * stride;
+			return new byte[dataSize];
+		}
+
+		private static RGBImage GetBitmapDataAsIs(BitmapData data)
+		{
+			var imageData = data.CopyRasterData();
+			return new RGBImage(data.Width, data.Height, data.Stride,imageData);
+		}
+
+		/// <summary>
+		/// Converts this image to Bitmap representation
+		/// </summary>
+		/// <returns></returns>
+		public Bitmap ToBitmap()
+		{
+			var pixelFormat = PixelFormat.Format24bppRgb;
+			var bitmap = new Bitmap(Width, Height, PixelFormat.Format24bppRgb);
+			var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, pixelFormat);
+			try
+			{
+				var newStride = bitmapData.Stride;
+				var oldStride = BytesPerLine;
+				if (newStride != oldStride)
+				{
+					var currentSourceOffset = 0;
+					var currentTargetLinePtr = bitmapData.Scan0;
+					for (var scanLineIdx = 0; scanLineIdx < Height; scanLineIdx++)
+					{
+						Marshal.Copy(ImageData, currentSourceOffset, currentTargetLinePtr, newStride);
+						currentTargetLinePtr += newStride;
+						currentSourceOffset += oldStride;
+					}
+				}
+				else
+				{
+					Marshal.Copy(ImageData, 0, bitmapData.Scan0, bitmapData.Height * newStride);
+				}
+			}
+			finally
+			{
+				bitmap.UnlockBits(bitmapData);
+			}
+
+			return bitmap;
+		}
+
+	}
+}
